d9b6481b51f7e41f0869f62ff5989a5ab54c7e89
d.evdokimov <d.evdokimov@astondevs.ru>
2024-05-15 19:32:51 +0300

    init

---
+++ src/main/java/api/invest/investapibackend/InvestApiBackendApplication.java
@@ -0,0 +1,13 @@
+package api.invest.investapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestApiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/invest/investapibackend/InvestApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.invest.investapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
