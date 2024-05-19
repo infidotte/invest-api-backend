9c37d69d9e97a81bac4292a5a20e38bc907421dd
infidotte <pflatershay@inbox.ru>
2024-05-19 14:57:16 +0300

    minor fixes

--- api-gateway/src/main/java/api/invest/entity/User.java
+++ api-gateway/src/main/java/api/invest/entity/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.UUID;
@@ -20,5 +21,6 @@ public class User {
     private String password;
     private Role role;
     private boolean enabled = false;
+    @Column("verify_code")
     private int verifyCode;
 }
